049b3ae2e6e1c0f7e914f3132f1aae5903cdfc1b
offic <offic@DESKTOP-GLF4UNU>
2024-07-10 15:27:52 +0600

    Types of inheritance.

--- src/main/java/oop_in_inheritance/OOP_in_Inheritance.java
+++ src/main/java/oop_in_inheritance/OOP_in_Inheritance.java
@@ -3,42 +3,35 @@ package oop_in_inheritance;
 public class OOP_in_Inheritance {
 
     public static void main(String[] args) {
-        System.out.println("Inheritance identity.");
+        System.out.println("Types of inheritance in object oriented programming_OOP.");
     }
     /*
-    object oriented programming_OOP:
-    object oriented programming প্রধানত ৪ প্রকার, 
-        Encapsulation
-        Inheritance
-        Polymorphism
-        Abstraction
-    
-    java-তে কাজ করার জন্য এই Access modifier গুলোর প্রয়োজন পড়ে,
-    There are four types of access modifier in java:
-        private
-        default
-        protected
-        public
-    Access modifier গুলো কোন variable / method এর আগে বসানো হয়।।
-    
-    ++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-    inheritance(উত্তরাধিকার) এর আলোচনা:
-      inheritance এর বৈশিষ্ট্য,
-        1. inheritance হলো যেখানে একটি class অন্য একটি class বৈশিষ্ট্য extends করার মাধ্যমে নিয়ে আসে।।
-        2. একটি class-এ কোন code create করা হয়ে গেলে, তা আর অন্য class-এ create করতে হয় না, extends করার মাধ্যমে সে class-কে call করলে হয়ে যায়।।
-        3. method overriding এর জন্য inheritane প্রয়োজন।।
-        4. একটি class এর সাথে অন্য একটি class এর relationship তৈরি করার জন্য inheritance প্রয়োজন।।
-        5. যে extends করে তাকে sub class বলে, যাকে extends করা হয় তাকে super class বলে।।
-        6. super class-কে extends করলে তার সবকিছু sub class-এ চলে আসে, যদিও দেখা যায় না।। 
-        7. extends করলে sub class থেকে super class-এর সবকিছু control করা যায়।।
-    
-
     There are 4 types of inheritance in object oriented programming_OOP.
-        1. Single inheritance.   
-        2. multilevel inheritance.  
-        3. Hierarchical inheritance. 
-        4. multiple inheritance.     
+        1. Single inheritane.  
+            এখানে ২টি class থাকবে, ১টি class অন্য class-কে extends করবে।।
     
+        2. multilevel inheritance. 
+            এখানে কয়েকটি class চেইন আকারে থাকতে পারে, 
+            ১টি class অপর class-কে extends করবে।। যেমন, A->B->C->D->E->F->G.
+            (A extends B, B extends C, C extends D, D extends E, E extends F).
+    
+        3. Hierarchical inheritance. 
+            এখানে কয়েকটি class থাকতে পারে,তবে চেইন আকারে নয়, 
+            এখানে ১টি super class থাকবে, অন্য সকল class ১টি class-কে extends করবে।। 
+            যেমন, A extends B, 
+                 A extends C, 
+                 A extends D.
+    
+        4. multiple inheritance. 
+            এটা java support করে না, যেমন, A extends B, 
+                                        A extends C. ৩নং এর মতো, এতোটুকু সম্ভব।।
+    
+             তবে D extends B, 
+                 D extends C. 
+            একই class ২টি class-কে extends করবে, এটা সম্ভব না।। 
+    
+       (object oriented programming এর interface অংশে এর সমাধান দেখুন।।
+
      */
 
 }
