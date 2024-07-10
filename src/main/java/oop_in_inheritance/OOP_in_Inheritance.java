0d7c3d591441f319b8579f67e3204979e59e8ec4
offic <offic@DESKTOP-GLF4UNU>
2024-07-10 15:08:49 +0600

    Inheritance identity.

--- src/main/java/oop_in_inheritance/OOP_in_Inheritance.java
+++ src/main/java/oop_in_inheritance/OOP_in_Inheritance.java
@@ -3,7 +3,42 @@ package oop_in_inheritance;
 public class OOP_in_Inheritance {
 
     public static void main(String[] args) {
-        System.out.println("Welcome! Introduction to \"object oriented programming_OOP\".");
+        System.out.println("Inheritance identity.");
     }
+    /*
+    object oriented programming_OOP:
+    object oriented programming প্রধানত ৪ প্রকার, 
+        Encapsulation
+        Inheritance
+        Polymorphism
+        Abstraction
     
+    java-তে কাজ করার জন্য এই Access modifier গুলোর প্রয়োজন পড়ে,
+    There are four types of access modifier in java:
+        private
+        default
+        protected
+        public
+    Access modifier গুলো কোন variable / method এর আগে বসানো হয়।।
+    
+    ++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    inheritance(উত্তরাধিকার) এর আলোচনা:
+      inheritance এর বৈশিষ্ট্য,
+        1. inheritance হলো যেখানে একটি class অন্য একটি class বৈশিষ্ট্য extends করার মাধ্যমে নিয়ে আসে।।
+        2. একটি class-এ কোন code create করা হয়ে গেলে, তা আর অন্য class-এ create করতে হয় না, extends করার মাধ্যমে সে class-কে call করলে হয়ে যায়।।
+        3. method overriding এর জন্য inheritane প্রয়োজন।।
+        4. একটি class এর সাথে অন্য একটি class এর relationship তৈরি করার জন্য inheritance প্রয়োজন।।
+        5. যে extends করে তাকে sub class বলে, যাকে extends করা হয় তাকে super class বলে।।
+        6. super class-কে extends করলে তার সবকিছু sub class-এ চলে আসে, যদিও দেখা যায় না।। 
+        7. extends করলে sub class থেকে super class-এর সবকিছু control করা যায়।।
+    
+
+    There are 4 types of inheritance in object oriented programming_OOP.
+        1. Single inheritance.   
+        2. multilevel inheritance.  
+        3. Hierarchical inheritance. 
+        4. multiple inheritance.     
+    
+     */
+
 }
