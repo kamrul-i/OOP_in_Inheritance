1bd2a98e29df241ab66ec8cd8f3d070e4fd8e5fd
offic <offic@DESKTOP-GLF4UNU>
2024-07-10 14:58:24 +0600

    Welcome! Introduction to "OOP in inheritance".

---
+++ src/main/java/oop_in_inheritance/OOP_in_Inheritance.java
@@ -0,0 +1,9 @@
+package oop_in_inheritance;
+
+public class OOP_in_Inheritance {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome! Introduction to \"object oriented programming_OOP\".");
+    }
+    
+}
